2085a60479691e0183aa5f9198e9a91d842c1168
2300030390-Suresh <2300030390@kluniversity.in>
2025-09-10 10:25:02 +0530

    first commit

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/cart";
+const API_URL = "http://ec2-54-87-175-27.compute-1.amazonaws.com:8081/api/cart";
 
 const API_BASE = "http://localhost:8081/api/cart";
 
